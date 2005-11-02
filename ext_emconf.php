51fa72e1bb1f79f2d9fa6cfea7d61f4b92d3ea13
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-02 09:45:36 +0000

    Raised version number to 1.0.0.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dkd_staticupload_winscp/trunk@575 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "dkd_staticupload_winscp"
-# 
-# Auto generated 17-08-2005 12:16
-# 
+#
+# Auto generated 07-04-2005 17:47
+#
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -31,8 +31,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => '',
 	'private' => '',
 	'download_password' => '',
-	'version' => '0.7.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"2353";s:15:"ext_php_api.dat";s:4:"a095";s:14:"ext_tables.php";s:4:"1de4";s:12:"cli/conf.php";s:4:"08b0";s:19:"cli/exec_bg_cli.php";s:4:"fcd5";s:19:"doc/wizard_form.dat";s:4:"3e13";s:20:"doc/wizard_form.html";s:4:"d715";s:20:"res/WinSCP3-test.ini";s:4:"c0a4";s:28:"res/dkd-application-test.ppk";s:4:"cd8b";s:32:"res/dkd-application-test.ppk.pub";s:4:"a502";s:42:"sv1/class.tx_dkdstaticuploadwinscp_sv1.php";s:4:"8041";}',
+	'version' => '1.0.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:6:{s:12:"ext_icon.gif";s:4:"1bdc";s:15:"ext_php_api.dat";s:4:"a095";s:14:"ext_tables.php";s:4:"69a2";s:19:"doc/wizard_form.dat";s:4:"3e13";s:20:"doc/wizard_form.html";s:4:"d715";s:42:"sv1/class.tx_dkdstaticuploadwinscp_sv1.php";s:4:"b12d";}',
 );
 
 ?>
